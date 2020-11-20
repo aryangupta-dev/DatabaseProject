cfcdce23bd814dafc37a49d655749b13d97a09ac
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-20 13:20:42 +0530

    Initial commit

---
+++ src/Connetion/DatabaseConnection.java
@@ -0,0 +1,35 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/20/2020
+ *   Time: 12:25 PM
+ *   File: DatabaseConnection.java
+ */
+
+package Connetion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    //url of the database.
+    //name of the admin of the database.
+    //password of the admin of the database.
+
+    public static final String URL="jdbc:mysql://localhost:3306/Transport";
+    public static final String USERNAME="root";
+    public static final String PASSWORD="";
+    public static Connection connection=null;
+
+    public DatabaseConnection(){
+        try {
+            connection= DriverManager.getConnection(
+                    URL,
+                    USERNAME,
+                    PASSWORD
+            );
+        } catch (SQLException throwables) {
+            System.out.println("Cannot connect to the database ,"+"Please check your credentials");
+        }
+    }
+}
---
+++ src/Data/City.java
@@ -0,0 +1,69 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/20/2020
+ *   Time: 12:24 PM
+ *   File: City.java
+ */
+
+package Data;
+
+import java.util.Objects;
+
+    public class City {
+        private String name;
+        private boolean traversed;
+        private int kilometres;
+
+        public City(String name,boolean traversed,int kilometres){
+            this.name=name;
+            this.traversed=traversed;
+            this.kilometres=kilometres;
+        }
+        public String getName(){
+            return name;
+        }
+
+        public int getKilometres() {
+            return kilometres;
+        }
+
+        public boolean isTraversed() {
+            return traversed;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setKilometres(int kilometres) {
+            this.kilometres = kilometres;
+        }
+
+        public void setTraversed(boolean traversed) {
+            this.traversed = traversed;
+        }
+
+        @Override
+        public String toString() {
+            return "City{" +
+                    "name='" + name + '\'' +
+                    ", traversed=" + traversed +
+                    ", kilometres=" + kilometres +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Data.City city = (Data.City) o;
+            return traversed == city.traversed &&
+                    kilometres == city.kilometres &&
+                    Objects.equals(name, city.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, traversed, kilometres);
+        }
+    }
